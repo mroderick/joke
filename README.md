51b60e9bacc942b094b9d338e28fec2893963078
Morgan Roderick <morgan.roderick@zalando.de>
2018-04-19 08:59:42 +0200

    Add certificates

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # joke
+
 Experiment with passport.js
+
+## Run on localhost
+
+1.  Add `127.0.0.1 joke.localhost` to `/etc/hosts` (you only need to do this once)
+1.  `npm install`
+1.  `npm start`
+1.  Visit https://joke.localhost:8443 in a browser
+1.  Ignore the security warnings, it's using a self signed certificate
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 "use strict";
 
+const https = require("https");
+const fs = require("fs");
 const express = require("express");
 const app = express();
+
+const options = {
+    cert: fs.readFileSync("./certs/localhost.crt"),
+    key: fs.readFileSync("./certs/localhost.key")
+};
+
+app.use(require("helmet")());
+
+app.use(express.static("static"));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/health-check", (req, res) => res.sendStatus(200));
 
-// eslint-disable-next-line no-console
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(8080);
+https.createServer(options, app).listen(8443);
